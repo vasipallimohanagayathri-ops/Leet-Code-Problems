3c74f58d341841fc7b123b5d497c1eeb2f81eb6b
vasipallimohanagayathri-ops <vasipallimohanagayathri@gmail.com>
2026-07-28 08:40:07 +0530

    Create 1221.Split a String in Balanced Strings.JAVA

---
+++ 1221.Split a String in Balanced Strings.JAVA
@@ -0,0 +1,22 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int balance = 0;
+        int count = 0;
+        for(char ch : s.toCharArray())
+        {
+            if(ch == 'L')
+            {
+                balance++;
+            }
+            else
+            {
+                balance--;
+            }
+            if(balance == 0)
+            {
+                count ++;
+            }
+        }
+        return count;
+    }
+}
